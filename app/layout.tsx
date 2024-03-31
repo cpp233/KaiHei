9d06c229adf6b1faa81848d73f34a7d1e1945e2e
cpp233 <tincint@foxmail.com>
2024-03-31 17:48:03 +0800

    导航栏

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,9 @@ import type { Metadata } from 'next';
 import { Open_Sans } from 'next/font/google';
 import { ClerkProvider } from '@clerk/nextjs';
 
-import { ThemeProvider } from '@/components/providers/theme-provider';
 import { cn } from '@/lib/utils';
+import { ThemeProvider } from '@/components/providers/theme-provider';
+import { ModalProvider } from '@/components/providers/modal-provider';
 
 import './globals.css';
 
@@ -29,6 +30,7 @@ export default function RootLayout({
             enableSystem
             storageKey='kaihei-theme'
           >
+            <ModalProvider></ModalProvider>
             {children}
           </ThemeProvider>
         </body>
