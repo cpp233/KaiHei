d0e39c62477726136bc8d8b20fecdacc15fddb0e
cpp233 <tincint@foxmail.com>
2024-04-02 21:07:33 +0800

    fix:数据库更新:消息

---
+++ lib/conversation.ts
@@ -0,0 +1,9 @@
+import { db } from '@/lib/db';
+
+const findConversation = async (memberOneId: string, memberTwoId: string) => {
+  return await db.conversation.findFirst({
+    where: {
+      AND: [{ memberOneId: memberOneId, memberTwoId: memberTwoId }],
+    },
+  });
+};
