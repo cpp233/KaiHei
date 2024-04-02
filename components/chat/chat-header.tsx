e87f9b8eb4a5a75df442b49a5f61ddf7f7e5d35e
cpp233 <tincint@foxmail.com>
2024-04-02 21:37:18 +0800

    服务器-内容区-聊天头部 & 聊天关系初始化

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,11 +1,12 @@
 import { Hash, Menu } from 'lucide-react';
 import { MobileToggle } from '@/components/mobiel-toggle';
+import UserAvatar from '@/components/user-avatar';
 
 interface ChatHeaderProps {
   serverId: string;
   name: string;
   type: 'channel' | 'conversation';
-  imageUrl?: 'string';
+  imageUrl?: string;
 }
 
 const ChatHeader = ({ serverId, name, type, imageUrl }: ChatHeaderProps) => {
@@ -14,6 +15,12 @@ const ChatHeader = ({ serverId, name, type, imageUrl }: ChatHeaderProps) => {
       <MobileToggle serverId={serverId}></MobileToggle>
       {type === 'channel' && (
         <Hash className='w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2"'></Hash>
+      )}
+      {type === 'conversation' && (
+        <UserAvatar
+          src={imageUrl}
+          className='w-8 h-8 md:h-8 md:w-8 mr-2 '
+        ></UserAvatar>
       )}
       <p className='font-semibold text-md text-black dark:text-white'>{name}</p>
     </div>
